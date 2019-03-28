614cb3c9ef1fab8f5eb9ba97d3dabd042e6564ef
Clayton Withrow <cwithrow@gmail.com>
2019-03-28 18:28:42 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+//App Components
+import Header from './Header';
+import Home from './Home';
+import Current from './Current';
+import Search from './Search';
+import Contact from './Contact';
+import Zip from './Zip';
+
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      {/* Creates routing between the different components of app. */}
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path ="/Current" component={Current} />
+      <Route path ="/Search" component={Search} />
+      <Route path ="/Zip" component={Zip} />
+      <Route path ="/Contact" component={Contact} />
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
